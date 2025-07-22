bff99844bf89b815fa9402b48bfd76cdb6867276
Husen <husenmalik7@gmail.com>
2025-07-22 14:50:26 +0700

    feat: get All Locations

--- src/server.js
+++ src/server.js
@@ -1,58 +1,12 @@
 require('dotenv').config();
 
-const Hapi = require('@hapi/hapi');
-const Jwt = require('@hapi/jwt');
-const Inert = require('@hapi/inert');
-const path = require('path');
-
-const HttpError = require('./exceptions/HttpError');
-const TokenManager = require('./tokenize/TokenManager');
-
-// albums
-const albums = require('./api/albums');
-const AlbumsService = require('./services/postgres/AlbumsService');
-const AlbumsValidator = require('./validator/albums');
-
-// categories
-const categories = require('./api/categories');
-const CategoriesService = require('./services/postgres/CategoriesService');
-
-// losts
-const losts = require('./api/losts');
-const LostsService = require('./services/postgres/LostsService');
-const LostsValidator = require('./validator/losts');
-
-// lost-comments
-const lostComments = require('./api/lost-comments');
-const LostCommentsValidator = require('./validator/lost-comments');
-
-// founds
-const founds = require('./api/founds');
-const FoundsService = require('./services/postgres/FoundsService');
-const FoundsValidator = require('./validator/founds');
-
-// found-comments
-const foundComments = require('./api/found-comments');
-const FoundCommentsValidator = require('./validator/found-comments');
-
-// users
-const users = require('./api/users');
-const UsersService = require('./services/postgres/UsersService');
-const UsersValidator = require('./validator/users');
-
-// authentications
-const authentications = require('./api/authentications');
-const AuthenticationsService = require('./services/postgres/AuthenticationsService');
-const AuthenticationsValidator = require('./validator/authentications');
-
-// uploads
-const uploads = require('./api/uploads');
-const StorageService = require('./services/storage/StorageService');
-const UploadsValidator = require('./validator/uploads');
+// prettier-ignore
+const { Hapi, Jwt, Inert, path, HttpError, TokenManager, albums, AlbumsService, AlbumsValidator, categories, CategoriesService, locations, LocationsService, losts, LostsService, LostsValidator, lostComments, LostCommentsValidator, founds, FoundsService, FoundsValidator, foundComments, FoundCommentsValidator, users, UsersService, UsersValidator, authentications, AuthenticationsService, AuthenticationsValidator, uploads, StorageService, UploadsValidator, } = require('./import');
 
 const init = async () => {
   const albumsService = new AlbumsService();
   const categoriesService = new CategoriesService();
+  const locationsService = new LocationsService();
 
   const lostsService = new LostsService();
   const foundsService = new FoundsService();
@@ -103,6 +57,12 @@ const init = async () => {
       options: {
         service: albumsService,
         validator: AlbumsValidator,
+      },
+    },
+    {
+      plugin: locations,
+      options: {
+        service: locationsService,
       },
     },
     {
